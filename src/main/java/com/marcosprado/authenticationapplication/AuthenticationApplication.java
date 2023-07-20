2ca582bd61c92bfb272b0fa12332d165641f2e54
marqosPrado <marcospradofelix@gmail.com>
2023-07-20 17:35:33 -0300

    going up project to remote repository

---
+++ src/main/java/com/marcosprado/authenticationapplication/AuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.marcosprado.authenticationapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthenticationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthenticationApplication.class, args);
+    }
+
+}
